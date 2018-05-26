35b10dc2f2e700e3e0136fbc65fdce4703ea995e
ljx0320 <lijiaxin@umich.edu>
2018-05-26 11:59:04 -0400

    data associations complete

--- Associations/references.js
+++ Associations/references.js
@@ -1,31 +1,10 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo_reference");
 
-// POST - title, content
-
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
-
-var Post = mongoose.model("Post",postSchema);
+var Post = require("./models/post");
 
 
-
-// USER - email, name
-
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"Post"
-        }
-    ]
-});
-
-var User = mongoose.model("User", userSchema);
+var User = require("./models/user");
 
 
 
@@ -37,8 +16,8 @@ var User = mongoose.model("User", userSchema);
 
 
 // Post.create({
-//    title:"How to cook part 3",
-//    content: "sdaddopwji"
+//    title:"How to cook part 4",
+//    content: "whatghost"
 // },function (error, post) {
 //     User.findOne({email:"bob@gmail.com"}, function (err, foundUser) {
 //         if (err) {
@@ -62,11 +41,11 @@ var User = mongoose.model("User", userSchema);
 // Find user
 // Find all posts made by that user, but not just IDs of posts
 
-User.findOne({email:"bob@gmail.com"}).populate("posts").exec(function(err,user){
-    if (err){
-        console.log(err);
-    }
-    else {
-        console.log(user);
-    }
-});
+// User.findOne({email:"bob@gmail.com"}).populate("posts").exec(function(err,user){
+//     if (err){
+//         console.log(err);
+//     }
+//     else {
+//         console.log(user);
+//     }
+// });
