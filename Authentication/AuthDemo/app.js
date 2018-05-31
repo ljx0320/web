539c45bd19effe3c8e787366aac8c1155f9d6eea
lijiaxin <lijiaxin@umich.com>
2018-05-31 18:11:36 +0800

    303 finished

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,34 +1,27 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var passport = require("passport");
-var bodyParser = require("body-parser");
-var LocalStrategy = require("passport-local");
-var passportLocalMongoose = require("passport-local-mongoose");
-var User = require("./models/user");
-
-
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 var app = express();
-
-
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
-    secret: "JOJO is the best!",
+    secret: "JOJO is the best",
     resave: false,
     saveUninitialized: false
 }));
 
-
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(User.serializeUser);
-passport.deserializeUser(User.deserializeUser);
-
-
-
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // ======================
 // ROUTES
@@ -40,7 +33,7 @@ app.get("/", function (req,res) {
 });
 
 
-app.get("/secret", function (req, res) {
+app.get("/secret", isLoggedIn, function (req, res) {
    res.render("secret");
 });
 
@@ -67,9 +60,30 @@ app.post("/register", function (req, res) {
 
 
 app.get("/login", function (req, res) {
-   res.render("login");
+    res.render("login");
 });
 
+// Login logic
+
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function (req, res) {
+});
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn (req, res, next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    else {
+        res.redirect("/login");
+    }
+};
 
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
